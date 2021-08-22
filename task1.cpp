027b86867e80175076fa3cb124a908f800b612d8
ahmadmuhtashim <89342417+ahmadmuhtashim@users.noreply.github.com>
2021-08-22 05:18:58 -0700

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,17 @@
+/*
+Name: Ahmad muhtashim
+Roll: BSEF20A047
+section:  Afternoon
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Name: Ahmad muhtashim\nbatch:BSEF20A047\nsection: Afternoon\nGPA:3.01";
+
+    return 0;
+}
